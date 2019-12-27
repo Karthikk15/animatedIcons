0682e43c4e1cb3e6d96da9b1f1805b32c093761b
Chinnu Muniyappan <chinnu.muniyappan@syncfusion.com>
2019-12-27 17:28:39 +0530

    added additional effect.

--- scripts/iconDemo.ts
+++ scripts/iconDemo.ts
@@ -1803,3 +1803,61 @@ calculatorIconBtn.addEventListener('click', function () {
  }
  calculatorIcon.animateElement(this, calculatorIconOptions, this.style.color, false);
 });
+
+class MoonStar extends mojs.CustomShape {
+    getShape () { return '<path d="M10.544,8.717l1.166-0.855l1.166,0.855l-0.467-1.399l1.012-0.778h-1.244L11.71,5.297l-0.466,1.244H10l1.011,0.778L10.544,8.717z M15.986,9.572l-0.467,1.244h-1.244l1.011,0.777l-0.467,1.4l1.167-0.855l1.165,0.855l-0.466-1.4l1.011-0.777h-1.244L15.986,9.572z M7.007,6.552c0-2.259,0.795-4.33,2.117-5.955C4.34,1.042,0.594,5.07,0.594,9.98c0,5.207,4.211,9.426,9.406,9.426c2.94,0,5.972-1.354,7.696-3.472c-0.289,0.026-0.987,0.044-1.283,0.044C11.219,15.979,7.007,11.759,7.007,6.552 M10,18.55c-4.715,0-8.551-3.845-8.551-8.57c0-3.783,2.407-6.999,5.842-8.131C6.549,3.295,6.152,4.911,6.152,6.552c0,5.368,4.125,9.788,9.365,10.245C13.972,17.893,11.973,18.55,10,18.55 M19.406,2.304h-1.71l-0.642-1.71l-0.642,1.71h-1.71l1.39,1.069l-0.642,1.924l1.604-1.176l1.604,1.176l-0.642-1.924L19.406,2.304z"></path>'; }
+    getLength () { return 200; }
+  }
+  mojs.addShape( 'moonStar', MoonStar ); 
+
+  class Star extends mojs.CustomShape {
+    getShape () { return '<path d="M17.684,7.925l-5.131-0.67L10.329,2.57c-0.131-0.275-0.527-0.275-0.658,0L7.447,7.255l-5.131,0.67C2.014,7.964,1.892,8.333,2.113,8.54l3.76,3.568L4.924,17.21c-0.056,0.297,0.261,0.525,0.533,0.379L10,15.109l4.543,2.479c0.273,0.153,0.587-0.089,0.533-0.379l-0.949-5.103l3.76-3.568C18.108,8.333,17.986,7.964,17.684,7.925 M13.481,11.723c-0.089,0.083-0.129,0.205-0.105,0.324l0.848,4.547l-4.047-2.208c-0.055-0.03-0.116-0.045-0.176-0.045s-0.122,0.015-0.176,0.045l-4.047,2.208l0.847-4.547c0.023-0.119-0.016-0.241-0.105-0.324L3.162,8.54L7.74,7.941c0.124-0.016,0.229-0.093,0.282-0.203L10,3.568l1.978,4.17c0.053,0.11,0.158,0.187,0.282,0.203l4.578,0.598L13.481,11.723z"></path>'; }
+    getLength () { return 200; }
+  }
+  mojs.addShape( 'star', Star ); 
+  
+  let moonStarBtn = document.getElementById('moonStar');
+  let moonStarIcon: AnimateIcon;
+    let moonStarOptions: any;
+    let moonStarSpan: any;
+
+    moonStarBtn.addEventListener('click', function () {
+        this.style.color = '#FFD700';
+        this.style.stroke='#FFD700';
+        if (typeof moonStarIcon === "undefined") {
+            
+            moonStarIcon = new AnimateIcon();
+            moonStarSpan = this.firstElementChild;
+            moonStarOptions = {
+                tweens: [
+
+                    new mojs.Burst({
+                        parent: this,
+                        radius: { 0: 10 },
+                        count: 11,
+                        children: {
+                            shape: 'star',
+                            fill: '#FFD700',                      
+                            stroke: '#FFD700',
+                            opacity: 0.6,
+                            radius: 80,
+                            duration: 3700,
+                            easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
+                        }
+                    }),
+
+                    new mojs.Tween({
+                        duration: 1200,
+                        onUpdate: function (progress) {
+                            moonStarSpan.classList.add('slideOutUp');
+                        }
+                    }),
+
+                ]
+            };
+        } else {
+            moonStarSpan.classList.remove('slideOutUp');
+            this.style.stroke=null;
+        }
+        moonStarIcon.animateElement(this, moonStarOptions, this.style.color, false);
+    });
